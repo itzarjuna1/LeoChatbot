7d8b81e6a9aa8dcd61315d314724d1b7f69909d0
II_CHALCOGEN_II <166417113+CHALCOGEN645@users.noreply.github.com>
2025-02-02 13:10:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "20318304"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "66b0a797adb0e779b57708117a6fe1f2"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", "6953998697"))
-SUPPORT_GRP = "Ur_support07"
-UPDATE_CHNL = "Ur_rishu_143"
-OWNER_USERNAME = "TheRishuCoder"
+SUPPORT_GRP = "sweetheart_hub"
+UPDATE_CHNL = "itz_soulmates"
+OWNER_USERNAME = "ll_malware_ll"
