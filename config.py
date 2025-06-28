afc2479f711af236444a4f262c673bb3b5cc0f09
ARJUN <pachangedinanath@gmail.com>
2025-06-28 14:42:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "20318304"
+API_ID = "22565342"
 # -------------------------------------------------------------
-API_HASH = "66b0a797adb0e779b57708117a6fe1f2"
+API_HASH = "75e035926f72f2f4155a6f5f6e64be03"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING1 = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6953998697"))
-SUPPORT_GRP = "sweetheart_hub"
-UPDATE_CHNL = "itz_soulmates"
-OWNER_USERNAME = "ll_malware_ll"
+BOT_TOKEN = getenv("8048266755:AAHQ6HNkXL_Vmt-LF46R2n_ARBeLkynVlYA", None)
+STRING1 = getenv("BQFYUd4AaBQ-7rxbKMguDsyH8oJPUtsDN4JBK4J-P09mTvQkHm24IsRuD3VtdQi1nbxvR97mcLHog1Ke9s6dKIYskILAyeaNzfr7CayVeDm_hiY0XKz9Ge7BR8A4KXBFdpUy_9KA7t0yDheKWcdJOM-ArBjs40uUUIjyPd0tntz3tWArfLmttWkuUNI2LGmTn_EjZjBH_XRopuZNVsesynDa7C5z8a-5V_Z87OKlzkoQL8u5Tw34EYMA_0eBsdjuEs5-rU2gz80eDSdIf9e0Pf7vWxy8tPxRjNe6DROsj5-m-CEys1SIoF_Uaw0jCZo8lZRRFk_ezLpnp2zclT0dk3iCxJDPeQAAAAHjZt-vAA", None)
+MONGO_URL = getenv("mongodb+srv://knight4563:knight4563@cluster0.a5br0se.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
+OWNER_ID = int(getenv("OWNER_ID", "7926944005"))
+SUPPORT_GRP = "https://t.me/dark_knight_support"
+UPDATE_CHNL = "https://t.me/dark_x_knight_musiczz_support"
+OWNER_USERNAME = "ll_KUZE_ll"
