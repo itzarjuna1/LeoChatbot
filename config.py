1359a9d2b0f54bde35c96011c1aae93cc99933ee
ARJUN <pachangedinanath@gmail.com>
2025-07-09 15:27:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "22565342"
 # -------------------------------------------------------------
 API_HASH = "75e035926f72f2f4155a6f5f6e64be03"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("8048266755:AAHQ6HNkXL_Vmt-LF46R2n_ARBeLkynVlYA", None)
+BOT_TOKEN = getenv("7732412587:AAFks0VQgQLI4-XzhvL13ksMRltG8opH34w", None)
 STRING1 = getenv("BQFYUd4AkVjGeCzSGzqd7OdkMyLtZ373VPK5ZFihSAgdfuS7ccs-kfHSGE1Lz5yvvXQroBg9Q_XQfmTvHgllIRlcZBwNdB4OQjRQmspcMBa5gFQCWN_4Vw7lhsA-6Hanfts29gWEUAfWJj1Lt4sI29a7ufq9k4oPThym31akZQBscakrugQf-Ed2QwCnORyOzn8-nTzZaVRduBjldci1doBx0n1d8prsS2xLTm9gBbYWsnVfZePFE5l0OJbTSz62w7vLM0qQPMjS6FKXllxkcriO5Dc5RGsKGVwJ-boT0bUrOqM4fV3jB_WLsKiEG7bv4jA-O0zYBgH7LRwU2gi7EGvGjy8ZgQAAAAHQda6jAA", None)
 MONGO_URL = getenv("mongodb+srv://knight4563:knight4563@cluster0.a5br0se.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
 OWNER_ID = int(getenv("OWNER_ID", "7926944005"))
